3f1a2cb5a2d85893547d2343916131cb40356bf2
Ocks <milomgenova@gmail.com>
2025-09-14 17:29:48 -0400

    enemy wave system

--- scripts/game/game.gd
+++ scripts/game/game.gd
@@ -4,6 +4,8 @@ extends Node2D
 @export var nerd_scene : Resource
 
 const STARTING_MUSIC = "res://audio/Food.mp3"
+var enemies_for_wave = -1;
+var current_wave = -1;
 
 func _ready() -> void:
 	# If the music player is not playing or if the music playing is not the starting music, play the starting game music
@@ -12,6 +14,7 @@ func _ready() -> void:
 		MusicPlayer.panning_strength = 0;
 		MusicPlayer.max_distance = 100000000;
 		MusicPlayer.play()
+	startwave(0)
 
 
 func spawn_mob():
@@ -19,13 +22,27 @@ func spawn_mob():
 	%PlayerLol._path().progress_ratio = randf();
 	add_child(mob);
 	mob.global_position = %PlayerLol._path().global_position;
-	
+
+func startwave(wave):
+	current_wave = wave;
+	enemies_for_wave = 5 + (3*wave);
+	%SubTimer.wait_time = (0.9 / ((0.15 * wave) + 1)) + 0.1;
+	%SubTimer.start();
 
 
 func _on_timer_timeout() -> void:
 	spawn_mob();
+	enemies_for_wave -= 1;
+	if (enemies_for_wave > 0):
+		%SubTimer.start();
+	else:
+		%WaveTimer.start();
 
 
 func _on_player_lol_died_lol() -> void:
 	%DeadMenu.visible = true;
 	get_tree().paused = true;
+
+
+func _on_wave_timer_timeout() -> void:
+	startwave(current_wave + 1);
