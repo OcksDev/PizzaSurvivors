64431af395b548285d3f319464f2e9f16d622cfc
Ocks <milomgenova@gmail.com>
2025-09-17 18:01:03 -0400

    Added wave display

--- scripts/game/game.gd
+++ scripts/game/game.gd
@@ -26,6 +26,7 @@ func startwave(wave):
 	enemies_for_wave = 5 + (3*wave);
 	%SubTimer.wait_time = (0.9 / ((0.15 * wave) + 1)) + 0.1;
 	%SubTimer.start();
+	%CurrentWaveDisplay.text = "Current Wave: " + str(current_wave+1);
 
 
 func _on_timer_timeout() -> void:
